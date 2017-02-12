c60df2c3836425e1ff9117ccca149cd1d198e1b0
Chris Atkin <atkinchris@gmail.com>
2017-02-12 17:16:26 +0000

    Change to pure function

--- index.android.js
+++ index.android.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {
   AppRegistry,
   StyleSheet,
@@ -25,23 +25,21 @@ const styles = StyleSheet.create({
   },
 })
 
-export default class MyApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    )
-  }
-}
+const MyApp = () => (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>
+      Welcome to React Native!
+    </Text>
+    <Text style={styles.instructions}>
+      To get started, edit index.android.js
+    </Text>
+    <Text style={styles.instructions}>
+      Double tap R on your keyboard to reload,{'\n'}
+      Shake or press menu button for dev menu
+    </Text>
+  </View>
+)
+
+export default MyApp
 
 AppRegistry.registerComponent('MyApp', () => MyApp)
