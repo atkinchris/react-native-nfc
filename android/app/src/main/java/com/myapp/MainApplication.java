6dea42bd1943822f998eb54e7bd387d04eca232e
Chris Atkin <atkinchris@gmail.com>
2017-02-14 08:46:03 +0000

    Lint Android

--- android/app/src/main/java/com/myapp/MainApplication.java
+++ android/app/src/main/java/com/myapp/MainApplication.java
@@ -1,16 +1,13 @@
 package com.myapp;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
-import com.myapp.nfc.*;
+import com.myapp.nfc.NfcPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,7 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
+      return Arrays.asList(
           new MainReactPackage(),
           new NfcPackage()
       );
@@ -40,6 +37,6 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+    SoLoader.init(this, false);
   }
 }
--- android/app/src/main/java/com/myapp/nfc/NfcModule.java
+++ android/app/src/main/java/com/myapp/nfc/NfcModule.java
@@ -2,14 +2,12 @@ package com.myapp.nfc;
 
 import android.widget.Toast;
 
-import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 public class NfcModule extends ReactContextBaseJavaModule {
     private static final String DURATION_SHORT_KEY = "SHORT";
